79ab1258b5f61dd08bd384a7a60421f475a95b3c
Sleepe005 <ytkholopov005@mail.ru>
2021-06-10 23:31:37 +0300

    create vk auth and get users status in vk (do not working)

--- Messanger/Messanger/Messanger/ModelVk.cs
+++ Messanger/Messanger/Messanger/ModelVk.cs
@@ -10,33 +10,42 @@ namespace Messanger
 {
     class ModelVk : INotifyPropertyChanged
     {
-        private string login;
-        private string password;
+        private string token;
+        private string status;
 
         public ModelVk()
         {
         }
 
         // value - значение, которое передаст нам пользователь && get - получает, set - устонавливает
-        public string Login { get => login;
-            set {
-                if (login != value)
+        public string Token
+        {
+            get => token;
+            set
+            {
+                if (token != value)
                 {
-                    login = value;
-                    OnPropertyChanged("Login");
+                    token = value;
+                    OnPropertyChanged("Token");
                 }
             }
         }
-        public string Password { get => password; set {
-                if (password != value) 
+
+        public string Status
+        {
+            get => status;
+            set
+            {
+                if (status != value)
                 {
-                    password = value;
-                    OnPropertyChanged("Login");
+                    status = value;
+                    OnPropertyChanged("Status");
                 }
             }
         }
 
 
+        // Главное не удалять
         public event PropertyChangedEventHandler PropertyChanged; //Событие, которое будет вызвано при изменении модели 
         public void OnPropertyChanged([CallerMemberName]string prop = "") //Метод, который скажет ViewModel, что нужно передать виду новые данные
         {
--- Messanger/Messanger/Messanger/ViewModelVk.cs
+++ Messanger/Messanger/Messanger/ViewModelVk.cs
@@ -12,75 +12,74 @@ using VkNet.Model.RequestParams;
 using System.Collections.ObjectModel;
 using System.Linq;
 using VkNet.Enums.Filters;
+using VkNet.Enums.SafetyEnums;
 
 namespace Messanger
 {
-    class ViewModelVk : INotifyPropertyChanged
+    class ViewModelVk
     {
-        private string accauntName { get; set; }
-        public ICommand auth { get; }
+        // Создание модели
         public ModelVk ModelVk { get; set; }
+
+        public ICommand auth { get; }
+        public ICommand viewstatus { get; }
         VkApi api = new VkApi();
 
         public ViewModelVk()
         {
+            // Создание экземпляра класса модели и команд
             ModelVk = new ModelVk();
             auth = new Command(Auth);
+            viewstatus = new Command(ViewStatus);
         }
 
+        // Токен - 761c422f5b5cfd9cb9b3fddc06a93271a6ff064eb342057659c6ede6fa4184f021ab773ada027eaa4f02e
+        // Мой Id - 261009498
 
-        // Авторизация ВКонтакте
+
+        // Получение токена для авторизации
         public void Auth()
         {
-            api.Authorize(new ApiAuthParams
-            {
-                ApplicationId = 7801128,
-                Login = Login,
-                Password = Password,
-                Settings = Settings.All
+            // Получение токена для авторизации
+            Device.OpenUri(new Uri("https://oauth.vk.com/authorize?client_id=7864647&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=status&response_type=token&v=5.131&state=123456"));
+            // TODO: взять ссылку по которой вк перенапрвляет пользователя и изъять из неё токен и id пользователя и провести авторизацию
+
+        }
+
+        public void ViewStatus()
+        {
+            // пока авторизация проходит здесь. Токен вставялет пользователь в определённое поле
+            api.Authorize(new ApiAuthParams {
+                AccessToken = Token
             });
 
+            // Получения стасуса пользователь по ID
+            ModelVk.Status = api.Status.Get(261009498).Text;
+            OnPropertyChanged("Status");
         }
 
-        // получение Логина
-        public string Login
+        // Обработка токена
+        public string Token
         {
-            get { return ModelVk.Login; }
+            get { return ModelVk.Token; }
             set
             {
-                if (ModelVk.Login != value)
+                if (ModelVk.Token != value)
                 {
-                    ModelVk.Login = value;
-                    OnPropertyChanged("Login");
+                    ModelVk.Token = value;
+                    OnPropertyChanged("Token");
                 }
             }
         }
 
-        // получения Пароля
-        public string Password
+        // Хз что тут должно быть
+        public string Status
         {
-            get { return ModelVk.Password; }
-            set
-            {
-                if (ModelVk.Password != value)
-                {
-                    ModelVk.Password = value;
-                    OnPropertyChanged("Password");
-                }
-            }
+            get { return ModelVk.Status; }
         }
 
-        // Получение имени аккаунта ВКонтакте
-        public string AccauntName
-        {
-            get { return accauntName; }
-            set
-            {
-                var p = api.Users.Get(new long[] { 1 }).FirstOrDefault();
-                accauntName = p.FirstName;
-            }
-        }
 
+        // Главное не удалять
         public event PropertyChangedEventHandler PropertyChanged; //Событие, которое будет вызвано при изменении модели 
         public void OnPropertyChanged([CallerMemberName]string prop = "") //Метод, который скажет ViewModel, что нужно передать виду новые данные
         {
